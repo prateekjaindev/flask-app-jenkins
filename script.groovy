8a8cb652cb2e0fc08141358b5763a7eb4cf1860a
Prateek Jain <prateekjain0495@gmail.com>
2021-09-25 20:29:03 +0530

    Update script.groovy

---
+++ script.groovy
@@ -0,0 +1,14 @@
+def buildImage() {
+    sh "ls"
+    sh "docker build -t flask-app ."
+}
+
+def pushImage() {
+    sh "docker tag flask-app prateekjain/flask-app:v1"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh "docker push prateekjain/flask-app:v1"
+    }
+}
+
+return this
